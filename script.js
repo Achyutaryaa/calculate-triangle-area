b85a35cbb0550204714875fad1520a36579d66f1
Achyutarya Raaz <59292426+Achyutaryaa@users.noreply.github.com>
2024-03-03 16:52:16 +0530

    Update script.js
    Adding feature to check triangle is valid or not.

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let sideB = document.getElementById("sideB");
 let sideC = document.getElementById("sideC");
 let result = document.getElementById("result");
 
+
 function calculateArea() {
-    let valueA = Number(sideA.value);
-    let valueB = Number(sideB.value);
-    let valueC = Number(sideC.value);
-    let sp = (valueA + valueB + valueC)/2;
-    let sqrResult = sp*(sp - valueA)*(sp - valueB)*(sp - valueC);
-    let area = Math.sqrt(sqrResult);
-    result.innerHTML = "Area = " + area.toFixed(2);
+    let valueSideA = Number(sideA.value);
+    let valueSideB = Number(sideB.value);
+    let valueSideC = Number(sideC.value);
+    
+    if(valueSideA + valueSideB > valueSideC && valueSideB + valueSideC > valueSideA && valueSideC + valueSideA > valueSideB) {
+        let sp = (valueSideA + valueSideB + valueSideC)/2;
+        let sqrArea = sp * (sp - valueSideA) * (sp - valueSideB) * (sp - valueSideC);
+        let actualArea = Math.sqrt(sqrArea).toFixed(2);
+        result.innerHTML = "Area = " + actualArea;
+    }
+    else {
+        result.innerHTML = 'Invalid triangle. Please enter valid side lengths.';
+    }
 }
